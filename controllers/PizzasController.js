df950600ea8d3f2deccd0b114095b83dc87d19a6
gustavorooxx <gust_chagas@outlook.com>
2021-07-28 21:58:30 -0300

    conteudo da view dinamica e rota botao ver mais para visualizar a pizza

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -1,5 +1,16 @@
+const pizzas = require('../database/Pizzas.json')
+
 module.exports = {
-	index: (req, res)=>{
-		res.render("index");
+	index: (req, res) => {
+		res.render("index", {pizzas});
+	},
+
+	show: (req, res) => {
+
+		// 1- carregar a pizza de id passado (pizzas.find)
+		var pizza = req.params.id;
+		// 2- Renderizar a view pizza passando a pizza encontrada
+		res.render("pizza", {pizza} )
+		
 	}
 }
--- routes/PizzasRouter.js
+++ routes/PizzasRouter.js
@@ -6,4 +6,8 @@ const PizzasController = require('../controllers/PizzasController');
 /* GET home page. */
 router.get('/', PizzasController.index);
 
+// rota para "Ver mais" das pizzas
+
+router.get('/pizzas/:id', PizzasController.show)
+
 module.exports = router;
